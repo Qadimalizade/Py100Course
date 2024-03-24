71452d42361cc566497f8ece3e723460994f4999
qadimalizade <qadimalizade@gmail.com>
2024-03-24 22:26:58 +0300

    24.03 Лаба 2

--- Условия и циклы/Лабораторные работы/Финансовая подушка безопасности/main.py
+++ Условия и циклы/Лабораторные работы/Финансовая подушка безопасности/main.py
@@ -2,7 +2,9 @@ money_capital = 20000  # Подушка безопасности
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
-
-# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
-
-print("Количество месяцев, которое можно протянуть без долгов:", ...)
+count = 0
+while money_capital >= 1:
+    spend = spend * increase + spend
+    money_capital = money_capital + salary - spend
+    count += 1
+print("Количество месяцев, которое можно протянуть без долгов:", count)
